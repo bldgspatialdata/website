d7ff317863cb64ff1944b0946090cf549c8bf486
Eli Pousson <eli.pousson@gmail.com>
2023-08-30 11:31:29 -0400

    Add utility functions for yml updates

--- R/utilities.R
+++ R/utilities.R
@@ -1,3 +1,24 @@
+index_yml <- function(txt = NULL) {
+  if (rlang::is_string(txt) && fs::is_file(txt)) {
+   txt <- readLines(txt)
+  }
+
+  c(2:(which(txt == '---')[2] - 1))
+}
+
+read_yml <- function(path, remove = FALSE) {
+  txt <- readLines(path)
+  yml_index <- index_yml(txt)
+
+  if (remove) {
+    return(txt[c((max(yml_index) + 2):length(txt))])
+  }
+
+  ymlthis::as_yml(
+    paste(txt[index_yml(txt)], collapse = '\n')
+  )
+}
+
 # Function for renaming files ----
 rename_files <- function(path,
                          pattern = "-",
@@ -16,4 +37,40 @@ rename_files <- function(path,
     )
 
   fs::file_move(path, new_path)
+}
+
+# Function for renaming files ----
+update_file_yml <- function(path,
+                            ...,
+                            method = "add") {
+  if (fs::is_dir(path)) {
+    path_info <- fs::dir_info(path = path, glob = glob)
+    path <- path_info[["path"]]
+  }
+
+  for (x in path) {
+    yml <- read_yml(x)
+
+    txt <- read_yml(x, remove = TRUE)
+
+    if (method == "add") {
+      new_yml <- ymlthis::yml_toplevel(
+        yml,
+        ...
+      )
+    } else if (method == "replace") {
+      new_yml <- ymlthis::yml_replace(
+        yml,
+        ...
+      )
+    }
+
+
+    updated_txt <- c(
+      ymlthis:::capture_yml(new_yml),
+      txt
+    )
+
+    writeLines(updated_txt, con = x)
+  }
 }
