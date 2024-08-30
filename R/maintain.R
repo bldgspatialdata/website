ad2b781a071fdd42d1291f2111bcf721b8128894
your name <eli.pousson@gmail.com>
2024-08-29 22:18:46 -0400

    Refactor to allow use of source w/o unexpected issues
    Wrap scripts in if (FALSE) {}

--- R/maintain.R
+++ R/maintain.R
@@ -1,38 +1,47 @@
 # Update imports in DESCRIPTION file ----
 
-# List imports in Quarto documents
-qmd_imports <- purrr::map(
-  c(
-    fs::dir_ls("slides", glob = "*.qmd"),
-    fs::dir_ls("weeks", glob = "*.qmd"),
-    # FIXME: exercises can't be parsed
-    # fs::dir_ls("exercises", glob = "*.qmd"),
-    fs::dir_ls("resources", glob = "*.qmd")
-  ),
-  attachment::att_from_qmd
-) |>
-  unlist() |>
-  as.character()
+#' List imports based on assorted directories
+#'
+list_ges668_imports <- function(...) {
+  # List imports in Quarto documents
+  qmd_imports <- purrr::map(
+    c(
+      fs::dir_ls("slides", glob = "*.qmd"),
+      fs::dir_ls("weeks", glob = "*.qmd"),
+      # FIXME: exercises can't be parsed
+      # fs::dir_ls("exercises", glob = "*.qmd"),
+      fs::dir_ls("resources", glob = "*.qmd")
+    ),
+    attachment::att_from_qmd
+  ) |>
+    unlist() |>
+    as.character()
 
-# List imports in R scripts
-r_imports <- purrr::map(
-  c(
-    fs::dir_ls("R", glob = "*.R"),
-    fs::dir_ls("slides/R", glob = "*.R"),
-    fs::dir_ls("exercises/R", glob = "*.R")
-  ),
-  attachment::att_from_rscript
-) |>
-  unlist() |>
-  as.character()
+  # List imports in R scripts
+  r_imports <- purrr::map(
+    c(
+      fs::dir_ls("R", glob = "*.R"),
+      fs::dir_ls("exercises/R", glob = "*.R")
+    ),
+    attachment::att_from_rscript
+  ) |>
+    unlist() |>
+    as.character()
 
-imports <- unique(c(qmd_imports, r_imports))
+  imports <- unique(c(qmd_imports, r_imports)) |>
+    sort()
+
+  imports
+}
 
 # TODO: Remove this temporary work around when it is no longer needed
-imports |>
-  sort() |>
-  paste0(collapse = ",\n") |>
-  clipr::write_clip()
+if (FALSE) {
+  imports <- list_ges668_imports()
+
+  paste0("    ", imports) |>
+    paste0(collapse = ",\n") |>
+    clipr::write_clip()
+}
 
 # FIXME: Figure out why att_to_desc_from_is is not working
 # attachment::att_to_desc_from_is(
@@ -42,3 +51,16 @@ imports |>
 #   normalize = FALSE,
 #   must.exist = FALSE
 # )
+
+
+# Review the YAML frontmatter for all of the pages
+if (FALSE) {
+
+  dplyr::bind_rows(
+    read_qmd_params("exercises"),
+    read_qmd_params("slides"),
+    read_qmd_params("weeks")
+  )
+
+
+}
