06f13bc21e5dc7cd30f9bdbad9e23a339c387c15
Eli Pousson <eli.pousson@gmail.com>
2026-04-08 18:00:32 -0400

    Update tidycensus example

--- examples/tidycensus.R
+++ examples/tidycensus.R
@@ -1,3 +1,19 @@
+# Introduction to the tidycensus package for accessing the U.S. Census Bureau API
+# See <https://www.census.gov/data/developers/data-sets.html> for available APIs
+# See <https://data.census.gov/> to access Census data via the website
+# Or check out <https://censusreporter.org/> for ACS data
+
+# Install packages required for this example
+# pak::pak(c(
+#   "tidyverse",
+#   "sf",
+#   "tidycensus",
+#   "tigris",
+#   "osmdata",
+#   "marquee",
+#   "arcgislayers"
+# ))
+
 # Load packages
 library(tidyverse)
 library(sf)
@@ -5,51 +21,101 @@ library(tidycensus)
 
 options(tigris_use_cache = TRUE)
 
+# Set global default theme for plots
 theme_set(theme_void())
-
-update_geom_defaults(
-  ggplot2::GeomSf,
-  list(color = "white", alpha = 0.85, linewidth = 0.05)
+theme_update(
+  theme(
+    plot.caption.position = "plot"
+  )
 )
 
+# Set global defaults for fixed aesthetics in `geom_sf`
+update_geom_defaults(
+  ggplot2::GeomSf,
+  list(color = "white", alpha = 0.75, linewidth = 0.03)
+)
+
+# Set geography and year for script
 state_name <- "Maryland"
 county_name <- c("Baltimore city", "Baltimore County")
+year <- 2023
 
-states <- tigris::states()
+# Get Census spatial data at multiple levels
+states <- tigris::states(year = year)
 
-counties <- tigris::counties(state = state_name)
+counties <- tigris::counties(state = state_name, year = year)
 
-tracts <- tigris::tracts(state = state_name, county = county_name)
+tracts <- tigris::tracts(state = state_name, county = county_name, year = year)
 
-block_groups <- tigris::block_groups(state = state_name, county = county_name)
+block_groups <- tigris::block_groups(
+  state = state_name,
+  county = county_name,
+  year = year
+)
 
-blocks <- tigris::blocks(state = state_name, county = county_name)
+blocks <- tigris::blocks(state = state_name, county = county_name, year = year)
 
-zctas <- tigris::zctas()
+# Look at Census boundary data
+glimpse(tracts)
 
+glimpse(block_groups)
+
+# Note that blocks has a GEOID20 column - but no GEOID column
+glimpse(blocks)
+
+# Compare nested admin boundaries
 ggplot() +
-  geom_sf(data = states, fill = "slateblue") +
+  geom_sf(data = states, fill = "wheat") +
   geom_sf(data = counties, fill = "slateblue2") +
   geom_sf(data = tracts, fill = "slateblue4") +
   coord_sf(
-    xlim = st_bbox(tracts)[c("xmin", "xmax")] + c(-0.35, 0.35),
-    ylim = st_bbox(tracts)[c("ymin", "ymax")] + c(-0.35, 0.35)
+    xlim = st_bbox(tracts)[c("xmin", "xmax")] + c(-0.25, 0.25),
+    ylim = st_bbox(tracts)[c("ymin", "ymax")] + c(-0.25, 0.25)
   )
 
-acs_vars <- load_variables(2022, "acs5")
+mapview::mapview(
+  list(
+    states,
+    counties,
+    tracts,
+    st_filter(block_groups, tracts[1, ])
+  )
+)
 
-edu_birth_place_src <- get_acs(
+# Load variable definitions from Census
+acs_vars_src <- load_variables(
+  year = year,
+  dataset = "acs5"
+)
+
+# Practice: find the variable for households with no internet access
+
+# Load data for a single variable and summary variable
+edu_birth_place_single_var <- get_acs(
+  geography = "tract",
+  state = state_name,
+  variable = "B06009_006",
+  summary_var = "B06009_001",
+  year = year
+)
+
+# Load data for a full table
+edu_birth_place_tbl <- get_acs(
   geography = "tract",
   state = state_name,
   table = "B06009",
-  # summary_var = "B06009_001",
+  year = year,
+  # Make sure to cache the results
   cache_table = TRUE,
+  # Optionally include geometry (returns an sf object)
   geometry = TRUE
 )
 
+# Load data for selected variables
 edu_birth_place_wide <- get_acs(
   geography = "tract",
   state = state_name,
+  # Rename variables by using a named vector
   variables = c(
     "Total_" = "B06009_001",
     "Less than HS_" = "B06009_002",
@@ -58,100 +124,106 @@ edu_birth_place_wide <- get_acs(
     "Bachelors_" = "B06009_005",
     "Graduate school_" = "B06009_006"
   ),
+  year = year,
   cache_table = TRUE,
-  geometry = TRUE,
+  # output defaults to "tidy"
   output = "wide"
 )
 
+# wide format data uses `E` suffix for estimate and `M` suffix for MOE
+glimpse(edu_birth_place_wide)
 
-edu_birth_place <- edu_birth_place_src |>
-  left_join(
-    acs_vars |>
-      select(name, label),
-    by = c("variable" = "name")
+# Format variable definitions
+acs_vars <- acs_vars_src |>
+  rename(variable = name) |>
+  mutate(
+    indent = str_count(label, "!!") - 1,
+    table_id = str_sub(variable, end = -5)
+  ) |>
+  mutate(
+    line_number = row_number(),
+    .by = table_id
   ) |>
   separate_wider_delim(
-    cols = label,
+    label,
     delim = "!!",
     names_sep = "_",
-    too_few = "align_start"
+    too_few = "align_end",
+    too_many = "drop"
   ) |>
-  st_as_sf() |>
+  mutate(
+    column_title = label_8,
+    .after = variable
+  ) |>
+  relocate(
+    starts_with("label"),
+    .after = everything()
+  )
+
+# Join formatted variables to estimate data
+edu_birth_place <- edu_birth_place_tbl |>
+  left_join(
+    acs_vars,
+    by = join_by(variable)
+  ) |>
+  # Get the first estimate and MOE for each tract
   mutate(
     estimate_total = first(estimate),
     moe_total = first(moe),
     .by = GEOID
   ) |>
   mutate(
-    pct_estimate = estimate / estimate_total,
-    pct_moe = moe_prop(estimate, estimate_total, moe, moe_total),
+    perc_estimate = estimate / estimate_total,
+    # moe_prop is one of a few helper functions for deriving a MOE
+    perc_moe = moe_prop(estimate, estimate_total, moe, moe_total),
     .after = moe
   )
 
-grad_students <- edu_birth_place |>
+# Practice: use get_acs get both county and block group level data for
+# no internet access
+
+grad_deg <- edu_birth_place |>
   filter(
     variable == "B06009_006"
   )
 
-grad_students |>
+grad_deg |>
   st_filter(tracts) |>
-  filter(estimate > 0) |>
   ggplot() +
-  geom_sf(data = tracts) +
-  geom_sf(aes(fill = pct_estimate)) +
-  scale_fill_continuous(labels = scales::label_percent()) +
-  theme_void()
+  geom_sf(aes(fill = perc_estimate)) +
+  geom_sf(data = counties[c(3, 5), ], fill = NA, linewidth = 0.3) +
+  scale_fill_continuous(
+    name = "% w/ grad deg.",
+    labels = scales::label_percent()
+  )
 
-var1 <- get_acs(
-  geography = "county",
-  state = state_name,
-  table = "B06009",
-  cache_table = TRUE
-)
-
-var2 <- get_acs(
-  geography = "county",
-  state = state_name,
-  table = "B23022",
-  cache_table = TRUE
-)
-
-# etc.
-
+# Multiple tables are not supported
 select_tables <- c(
   "B06009",
   "B23022",
-  "B05001",
-  "B06009",
-  "B20004",
-  "B08012",
-  "B08133",
-  "B25091",
-  "B22002",
-  "B07009"
+  "B05001"
 )
 
 table_data <- get_acs(
   geography = "county",
   state = state_name,
   table = select_tables,
+  year = year,
   cache_table = TRUE
 )
 
-letter_fn <- function(x, ...) {
-  # here is the body
-}
-
+# `map` allows iterating over a vector
 map(
-  letters,
+  LETTERS,
   \(x) {
-    paste0(str_to_title(x), " is a letter")
+    str_glue("{x} is a letter")
   }
 )
 
+# Naming the input vector means the output keeps the names
 select_tables <- set_names(
   select_tables,
-  select_tables
+  nm = select_tables
 )
 
 acs_data_list <- map(
@@ -161,14 +233,20 @@ acs_data_list <- map(
       geography = "county",
       state = state_name,
       table = x,
+      year = year,
       cache_table = TRUE
     )
   }
 )
 
-acs_data_bind_rows <- bind_rows(acs_data_list)
+# acs_data_list is a list of data frames
+str(acs_data_list)
 
-acs_data_list_rbind <- list_rbind(acs_data_list, names_to = "table")
+# Combine data frame lists by row with purrr::list_rbind
+acs_data_list_rbind <- list_rbind(
+  acs_data_list,
+  names_to = "table"
+)
 
 # Combining ACS data with OSM data
 library(osmdata)
@@ -179,61 +257,50 @@ q <- opq_osm_id("relation/12867319")
 campus_osm <- osmdata_sf(q)
 
 campus <- campus_osm$osm_multipolygons |>
-  st_transform(crs = st_crs(grad_students))
+  st_transform(crs = st_crs(grad_deg))
 
-grad_students |>
-  st_filter(campus) |>
+grad_deg |>
+  # Filter data to area w/in 1.5 miles of UMBC campus
+  st_filter(
+    st_buffer(campus, dist = units::set_units(1.5, "mi"))
+  ) |>
   ggplot() +
-  geom_sf(aes(fill = pct_estimate)) +
-  geom_sf(data = campus, fill = NA, linewidth = 1.5)
-
-# Demonstration of dm package
-
-library(dm)
-
-tigris_dm <- dm(states, counties, tracts, block_groups)
-
-tigris_dm |>
-  dm_add_pk(states, GEOID) |>
-  dm_add_pk(counties, GEOID) |>
-  dm_add_pk(tracts, GEOID) |>
-  dm_add_pk(block_groups, GEOID) |>
-  dm_add_fk(states, GEOID, counties, STATEFP) |>
-  dm_add_fk(states, GEOID, tracts, STATEFP) |>
-  dm_add_fk(states, GEOID, block_groups, STATEFP) |>
-  dm_add_fk(counties, COUNTYFP, tracts, COUNTYFP) |>
-  dm_add_fk(counties, COUNTYFP, block_groups, COUNTYFP) |>
-  dm_add_fk(tracts, TRACTCE, block_groups, TRACTCE) |>
-  dm_draw(
-    view_type = "all"
+  geom_sf(aes(fill = perc_estimate)) +
+  geom_sf(data = campus, fill = NA, linewidth = 0.6) +
+  scale_fill_viridis_c(
+    name = "% of adults w/ grad or professional deg.",
+    labels = scales::label_percent()
+  ) +
+  labs(
+    caption = "Source: 2019-23 ACS 5-yr. Est. Table B06009.
+    Adults >=25 years old included in table population."
+  ) +
+  theme(
+    legend.title = marquee::element_marquee(
+      width = unit(0.2, unit = "npc")
+    ),
+    plot.caption = marquee::element_marquee(
+      width = unit(0.8, unit = "npc"),
+      hjust = 0,
+      margin = margin(b = 12)
+    )
   )
 
+# Interpolating ACS data
 library(arcgislayers)
 
-parks <- arc_read(
-  "https://bcgisdata.baltimorecountymd.gov/arcgis/rest/services/Recreation/ParkBoundaries/MapServer/1"
+council_districts <- arc_read(
+  "https://bcgisdata.baltimorecountymd.gov/arcgis/rest/services/Political/Political/MapServer/31"
 )
-
-park_area <- parks |>
-  st_buffer(dist = units::set_units(0.25, "mi")) |>
-  st_union() |>
-  st_as_sf() |>
-  mutate(
-    name = "Baltimore County Park Areas"
-  ) |>
-  st_transform(crs = st_crs(grad_students))
 
 blocks <- blocks |>
   st_transform(crs = st_crs(grad_students))
 
-grad_student_park_area_interp <- interpolate_pw(
+grad_student_council_district <- interpolate_pw(
   from = grad_students,
-  to = park_area,
+  to = council_districts,
   extensive = TRUE,
   weights = blocks,
   crs = 3857,
   weight_column = "POP20"
 )
-
-grad_student_park_area_interp |>
-  select(!contains("moe"))
